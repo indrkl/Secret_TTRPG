a870f33f9d198158e9ce4eab2e9ef01f863c3f04
indrkl <indrkl@gmail.com>
2025-06-24 22:32:32 +0300

    changes and stuff

---
+++ TTRPG/Campaign/Godherja/concrete_setup.py
@@ -0,0 +1,96 @@
+thoughts = """
+The currently existing agents:
+
+Empress of Tithoria, named Ezera from house Eros. She is theyoungest daughter and child of Emeros of house Eros, the legendary adventurer, conqueror, and emperor.
+
+They have changed the once Samirian empire into a Thyssian religion center, it is by now the most advanced civilization in the world. Their heavy infantry are unbeatable,
+their cities are rich and fully developed, their center, Astrologicon, is a metropolis unlike any other.
+
+Their main thesis is, that we must do better, they have banished blood and fog magic and seek through diplomacy to get others to follow suit. However after the death of 
+Emeros, his daughters have spent most of their energy to keep the realm together, since they are far from legendary as their father was, 
+and even in that they have only succeeded, because
+their father did manage to unite the realm in at least in religion, he managed to integrate his own Averserian culture with the Sardinian local culture, forming a new
+Thyssian culture. The main pillar of the culture, and the religion, is that the main pillars lie in rejection of the blood magic, and studying of nature, philosophy, human
+anatomy, medicine and all ancient knowledge that one could get their hands on. Only through patience, and knowledge can we find salvation!
+
+Mean while the Chevalier culture, bound by the Conclave of Raocourt religion, the dominant life path religion, has grown stronger since the Lich invastion, and now rule over many of the Svaljolki lands. They are relatively friendly towards Tithoria
+due to Emeros having come to their aid in a time of need against the Arch lich invastion, which scarred the once Chevalier homelands. However they are still not united,
+and they are no longer the most advanced civilization in Areas. Their civilization split between 5 Kingdoms: Perignan, ruled by Ruodin of house Guradt, 
+Heartwood ruled by Alogne of house Augue, Oudemarchs ruled by Remierch of house Pouillard, Aekershaan ruled by Magneric of house Hidushra and Conemaigne ruled by Thielbe of
+house Braecks.
+
+Since the Svaljolki invasion around 1250 - 1270 when they conquered and settled large parts of Western, southern and eastern Aversaria, they have largely lost control,
+but some Svaljolki rulers still rule in Western Averseria, their cultural impact is however much larger, but the elite of all other cultures finds it hard to trust them,
+due to their willingness to delve into fog magic.
+
+Finally the Averserian culture, once spanning the majority of the northern continent, it's only pure form descendant is the kingdom Hyrea, ruled by a powerful mage and
+conqueror, starting from a single county, Archythea of house Mischas has renewed the old ways, by using blood magic, schemeing, and sheer will and ambition to defeat 
+opponent after opponent. She doesn't think much of the ban on blood magic. She sees Tithoria as a decadent threat, that she hopes to one day deal with. But the truth is,
+as a civilization, the last holding of Eastern Averseria, is far behind in both development, culture and understanding of the world. What they lack in these, Archythea is
+willing to make up in magic.
+
+Then you have the nomads of Khorinots, the empire of Anokryosae, the turbulant mess in the red lands, and many more states, which however lack in both vision and ambition,
+to make a lasting impact to this world, at least for now.
+
+Things for players to discover:
+
+Why is magic twisted?
+
+The truth of the matter is, that it is not the magic that is twisted, but it is the souls of man that is weak, and cannot actually deal with the pressure of the magic.
+The depth of the misunderstanding is that, before Godherja, the pressure was constant in the world and much stronger, similar to the fog now. THe souls were however much
+stronger and clearer in their purpose, making them more resilient against the pressure. The Godherja removed that pressure allowing for souls to survive and florish while
+becoming weaker and more corrupt. However the moment of casting spells still creates this pressure and therefore the weaker souls break under it more consistently.
+
+What is the fog? 
+
+The last casting of the blood-replicating spell ripped the barrier that was keeping the old God out of this world a little bit open, so a portion of his magical pressure
+slipped through, creating the fog. Those who can forge their souls to be purer can endure the fog, and not go crazy. Even more, it appears as fog for only the normal men,
+who would not be able to survive the magical pressure the fog would apply.
+
+How to survive in fog, how to heal their soul?
+
+The damage to ones soul is actually much more fluid than damage to the body. A person could consistently heal or damage it by living either a clear way, or falling into
+chaos, fear and paranoia. The true virtues in this case are the clarity of mind, knowing what you want, where you want to go, and unwavaringly go to that direction. That
+means it is safe guarding your soul from fear of obstacles, considerations regarding other people and their conflicting desires, but also, the most complicated, avoiding
+paradoxes in your vision.
+
+Who is the Ice queen?
+
+She is a immortal human, who has such a soul, which is pure, so she lives deep in the fog near the dead God. She has a small town of people, who also can endure the fog.
+Also a lot of spirits, who ascended during their life times chill around there, not causing any trouble, but instead it is a mini sort of after-life for them, after
+Frodbrokna.
+
+TODO: What is her agenda now?
+
+Where do the Liches come from? 
+
+The magic for enternal life was given to ambitious mages by the Ice queen, though now she sees them as being too unworthy, not that she regrets anything. 
+
+What was the blood replicating spell?
+
+We know that people exploded, especially Magi, it's as if their blood boiled instantly, it was grotesque, we also know that there has been 2 instances of using of this spell
+that we know, once in Samirian empire long time ago, and second in Averserain empire that caused the Frodbrokna. We know it is very hard to control spell, and Aescherus
+failed to control it. We know that this caused the old God to stir and release the fog.
+
+What if it turns out that this has a connection to the old god, it is a distorted version of the creation spell, much weaker, much less potent, but still a direct connection
+to the old god, the original goal of the spell is to create a person, to shape their blood and what the creature is. The distorted version is just a version of a toddler
+wiping out a creature with God's powers, by not having the vision of what kind of creature you want to shape, you just distort the being into unstable garbage, throug that
+killing them, this spell has a direct connection to old god, and an attempt to further cast this spell may awaken the Old god for good, or turn you into a new version of it,
+or something else entirely, but in the hands of a stupid mortal, it is just a way to wipe out civilizations.
+
+Where did it come from?
+
+It was left by the old god himself, he taught it to one of his favourite Aelfirs, with the hope that he would one day create new life. In his vision, that day would 
+eventually come, even if this would be used many more times before that to cause terrible damage. 
+
+
+Mechninations for players to fight against, resist?
+
+Aid in war one faction. help fight against Second Arch-lich invasion, Help against the Fog eater invasion. But also find and stop schemes of Arch lich cultists and agents,
+or red lander schemes. Stop the redlanders from assassinating world leaders, or stop them in their quest for the world ending spell.
+
+
+
+
+
+"""
