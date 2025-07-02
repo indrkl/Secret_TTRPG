be1f48a33a55423277c713a617f831be2b0594bf
indrkl <indrkl@gmail.com>
2025-07-02 17:51:56 +0300

    Update ideas.py

--- TTRPG/Campaign/Godherja/ideas.py
+++ TTRPG/Campaign/Godherja/ideas.py
@@ -1,53 +1,87 @@
 Questions = """
 
-1. Which of these gets you most excited: Fighting monsters, learning about lore, social interactions, being part of
+1. Which of these gets you most excited: Fighting monsters, learning about lore, investigating a mystery, social interactions, being part of
 large battles, royal court drama and intrigue, messing with international politics.
 
 2. Which of these resonates with you? Being a mercenary, being scholars, being part of a secret order, being monster
 hunters, being part of a noble house.
 
 3. Which reward gets you going? A flaming magical sword, a book about ancient secrets, lot's of gold, a title of 
-baron and land that comes with it, A cool NPC joins your party, means to teleport between 2 places in the map.
+baron and an estate that comes with it, A cool NPC joins your party, means to teleport between 2 places in the map.
 
-4. How do you think you would enjoy the campaign to be structured? For example, do you prefer a concrete main quest,
-with occasional side quests, or maybe you prefer more disjointed adventures that all advance some metric, like 
-knowledge, wealth, fame.
 """
 
+intro_v2 = """
+Imagine, you are walking on the walls of the keep of Oraispol. The keep is rather in the center of the city, connected to the harbor through additional walls crawling down
+the hill. You see the harbors in the north west, you see the mighty Colosseum in the east, large communal areas all around. One, largest of the communities stands out, 
+Svaljolki community, they still reside around the old statue of Cenware the witch-breaker. His lineage, however gone from their former glory.
 
+Oraispol has come a long way from the civil war, 223 years ago, from Frodbrokna, that destroyed the empire, and left all it's cities to famine, chaos, plague, ruin. That
+was followed by the conquest on Cenware in 1258. His decendant managed to hold onto it for almost 2 decades before being conquered by Chevalier king in 1258, that was 
+then granted to a minor vassal and it passed from house to house, until Emeros Eros conquered it for the house Eros in 1395 whom have now controlled it ever since. 
+Oraispol has seen all the pages that have been turned in the world stage. 
+
+This city has seen many rulers talk about the need to do better, to find salvation for humanity. But true salvation, this still feels far away, there are so many questions,
+still, what is this fog, where did it come from? Will it ever go away? Can we heal the land that has been devastated by the fog? What about the land devastated by the 
+liches? What about the lands devastated by the war of the mages that led to Frodbrokna? Is the fog even greater threat to us, than we ourselves are?
+
+Thysian rulers want to abolish blood and fog magic. While svaljolki are
+not the most magically attuned, those few who are, often walk the path of the silentspeak, and dabble with both fog magic, but sometimes as well with blood magic. This
+causes tensions. The remaining Chevalier nobility, while treated well and there is some shared understanding with the Thysian nobility, they too remember the time when they
+ruled this city. They still tell stories of the times when Chevalier was the most advanced of the civilizations, right after Frodbrokna, by managing to sustain least damage,
+that is no more, Thysians have made their homeland in the heart of the old Samirian empire, and within 60 years since Emeros Eros conquerred that, it has become far more
+advanced than anything this world has to offer. We are not there however, we are in the other continent, in the northern continent. 
+Oraispol is the trade hub for the empire, a staging ground for projecting it's power to the
+northern continent, and also a safe haven for trade ships coming from the southern continent, a strategic crossroads for the world.
+
+Svaljolki emphasize the purity of the heart, bravery, strength, as if Aescheres was lacking either of these? There is however a more grounded, more peace of mind, more
+content posture to the Svaljolki, to go along with their bravery, their strength. Maybe it is different after all?
+
+But the old Aversarian ambition is once again rising in the east. You've heard whispers of a powerful mage Archythea Mishcas, who doesn't seem to lose any battle, and has
+been conquering few duchies at a time, forging a great kingdom of Hyrrea. She spits on all the wisdom of Thysians, of the restrictions of Chevalier, the peasant Svaljolki,
+she will have her blood magic, luckily for us, she doesn't come even close to Aescheres, but she is the mightiest of mage kings the world knows right now. And with her
+restoring the blood-line crafting to get the best mage babies possible, who knows where it will lead.
+
+The fog has been quiet for 50 years, though some whipser, this will change soon, but hasn't there always been someone whispering it? A broken clock is still right twice
+a day, a so too have the whispers been right before.
+
+You walk on this wall, pondering these thoughts, as the city lives it's busy life, gossipping about how naive the duke is, Thysian spies looking for the fog witches, there
+is at least 1 hanging, every month, the svaljolki unrest grows, but some other appeasements have kept it at bay, so far. The fights in the colosseum, forge mighty warriors,
+and the market, all sort of exotic goods flow through here, as this is the nexus, between south and north, you want something real bad, you come here, you snatch it, and
+it may not even reach any of the major capitals, this city, does indeed have it's unique power.
+"""
 
 intro_txt_pre_hooks = """
-Aeras is a broken world, Godherja, the first event that broke it thousands of years ago, Frodbrokna, the second event
-that broke it 58 years ago. And to be fair, for the men walking on the surface of Aeras, it wasn't nice before either
-of the events either.
+Aeras is a broken world, the latest breaking of the world happened in year 1200, which is now 223 years ago, Frodbrokna, that made half of the
+northern continent inhabitable. This started the age of changes, where the old empires, Averserian in the northern
+continent, and Samirian empire in the southern continent, both falling into depths of history. Svaljolki tribes
+migrated to the remainder of the Aversarian empire, wiping out it's culture, religion, people. Only a single small
+kingdom retained, having perhaps 1 / 20-th of the area of the entirety of the empire.
 
-Godherja, the god wars killed a whole other superior race of Aelfirs, immortals who had been God-kings to man-kind for
-as long as we had history records. All of them, wiped out, now only humen remain. But the true disaster came from a
-spell that also supposedly killed the creator of man, and Aelfir, both. He was named Aersanon. After Godherja, all
-civilization was lost, the world, nature and magic was twisted and made more hostile, but the few humen who remained,
-persevered, multiplied and rebuilt the world, forming many new cultures, religions, kingdoms, empires.
+The rest of the northern empire, is now held in prison by the fog. There, no civillization that we could relate to, exists.
+There are rumors of fog eaters, crazed humen, who can somehow survive there, and 50 years ago they launched an invasion that was
+pushed back by the joined forces of Chavelier kings and then alive legendary hero, adventurer, and in the end, emperor of Titheria,
+Emeros, founder of the house Eros. 20 years before that they also fought together against the Arch Lich invasion from the north of
+Chevalia. But the great hero is now dead. Her children, while perfectly capable and above average, still pale in comparison.
 
-Up until 58 years ago, the most powerful of such empires was Aversaria. It was in a civil war, being fought by 2
-extremely powerful mages. Similar power was not known in the recorded history, at least for humans. They raced to
-find a powerful spell that would end the other side, Aeschraes, managed to find it sooner, and sacrificed his family,
-hundreds of servants and several minor mages to cast it. But it destroyed much more than he had intended. Wiping out
-cities, regions, and releasing fog from the grave of Aersanon, the dead god. That fog would engulf more than half of the
-continent, making north and center completely unhinhabitable. The empire of Aversaria, half of it destroyed, but
-without a clear ruler, split into pieces. Sjalvolki tribes escaping the fog, united under Cenware the Witch-breaker,
-now marching to take over all the remaining lands of Aversaria. Aeschraes is the only powerful mage still left in the
-world, but he, considered as world-ender, is alone, and will soon face his demise as well. All other more powerful mages
-died during Frodbrokna, the releasing of the Blood-replicating spell by Aeschraes.
+Samirian empire in the south, was ended by the great adventurer Emeros Eros, who started conquering land there at the age of 80.
+He managed to do so for another almost 50 years, before he died at the really old age of 128. He and his family all claim he
+lived that long without the aid of any magic, but the rest of the world finds it hard to believe. During his rule, he solidified
+Thysian faith to be the dominant faith in the southern continent.
 
-In the north west, the Chevalie empire, guardians against the Lichdoms in the north is split by 3 brothers, none of them
-having enough might and legitimacy to unite it once again.
+The Titherian empire's one main idea to avoid future cataclysms was to ban blood and fog magic. And it seeked alliances with many of
+the Conclave of Raocourt kings who also had quite a disposition towards magic. Some of the sects in that religion even focus on anti-magic
+witch and mage hunting, though Titherians are not so extreme.
 
-In the east, there are less advanced lands, they too have no answer against the fog.
+However after Emeros Eros passed, his daughters have mainly focused on keeping the realm in tact. In the mean while, the last Aversarian
+house Mishcas, rises from the ashes, led by Blood mage Archythea and starts conquering great territories. 
+She is a great mage, and refutes the anti-magic coalition, and aims 
+to undermine and destroy it. She uses blood magic to summon armies of blood elementals, and together with conventional forces, remains
+undefeated in battlefield, and while she could not have matched Emeros Eros, he, is no longer alive.
 
-In the redlands, the cultist have become more active, and present another dangerous, nihilistic potential for world
-ending.
-
-Is there anyone to stop the Fog? To forge a better world? Is Aeras doomed to just slowly wane away? Is there no hope
-for people without a god?
+In the meanwhile, there are many good souls, still wondering, what will come of this world? What is this fog, and can these lost territories
+be recovered by man? How to avoid another magical disaster? What has really happened in the past? How did we get here? 
+Are we progressing at all as a civilization or are we just going around in circles?
 """
 
 campaign_hooks = """
