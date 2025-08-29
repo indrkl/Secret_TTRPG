4b7af24b4e38516ce1f74b876e80232a7231ec57
indrkl <indrkl@gmail.com>
2025-08-29 17:23:22 +0300

    changes
    change

--- TTRPG/Campaign/Godherja/Canon.py
+++ TTRPG/Campaign/Godherja/Canon.py
@@ -5,8 +5,20 @@ items_in_world = {
 }
 
 actions_that_have_been_done = [
+    """
+    Party saved the Thysian holy site from a ghost that was causing trouble there.
+    """,
     """Melly offered to sell wine from Asha, and he promised to send a shipment expecting to recieve 1000 ducats upon arrival.
     """,
     """Gudoric told Lilith that he is going to look for a buyer for the chalice he expects you to extort from Routund.
+    """,
+    """Melly met Rene and Henry, in a game of cards, Melly won the room Rene was staying in, but agreed to return his comfort
+    for a small favour in the future, something that would not put him in danger.
+    """,
+    """
+    Party defeated a group of bandits and returned the horses and the stolen gold to the villagers in the sieged county.
+    """,
+    """
+    Party met Brother Gurth.
     """,
 ]
